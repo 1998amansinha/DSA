e35bcbf2e9a5a23cd7e0afe0eae48ca32d01b549
[Aman Sinha] <1998amansinha@gmail.com>
2024-12-05 11:46:54 +0530

    code commit

--- Fibonacci.js
+++ Fibonacci.js
@@ -9,3 +9,5 @@ function fibonacci(n) {
 console.log(fibonacci(3)); // [ 0, 1, 1 ]
 console.log(fibonacci(5)); // [ 0, 1, 1, 2, 3 ]
 console.log(fibonacci(7)); // [0, 1, 1, 2, 3, 5, 8]
+
+// Time Complexity - O(n)
---
+++ Prime.js
@@ -0,0 +1,21 @@
+function prime(n) {
+  if (n < 2) {
+    return false;
+  }
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(prime(1));
+console.log(prime(2));
+console.log(prime(4));
+console.log(prime(5));
+console.log(prime(6));
+console.log(prime(7));
+console.log(prime(9));
+console.log(prime(10));
+console.log(prime(11));
