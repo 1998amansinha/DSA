b33e215961bed878be5e23fb9de93dc4e4f9b9e9
[Aman Sinha] <1998amansinha@gmail.com>
2024-12-10 12:28:25 +0530

    Quick Sort

---
+++ Sorting/QuickSort.js
@@ -0,0 +1,18 @@
+function quickSort(arr) {
+  if (arr.length < 2) {
+    return arr;
+  }
+  let pivot = arr[arr.length - 1];
+  let leftArr = [];
+  let rightArr = [];
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] < pivot) {
+      leftArr.push(arr[i]);
+    } else {
+      rightArr.push(arr[i]);
+    }
+  }
+  return [...quickSort(leftArr), pivot, ...quickSort(rightArr)];
+}
+
+console.log(quickSort([-6, 20, 8, -2, 4]));
