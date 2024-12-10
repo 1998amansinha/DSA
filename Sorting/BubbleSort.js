1be2f52c4d0850105bdf4e96d5309b12bcea02b6
[Aman Sinha] <1998amansinha@gmail.com>
2024-12-10 11:12:50 +0530

    Bubble Sort

---
+++ Sorting/BubbleSort.js
@@ -0,0 +1,33 @@
+// function bubbleSort(arr) {
+//   let swap;
+//   do {
+//     swap = false;
+//     for (let i = 0; i < arr.length - 1; i++) {
+//       if (arr[i] > arr[i + 1]) {
+//         let temp = arr[i];
+//         arr[i] = arr[i + 1];
+//         arr[i + 1] = temp;
+//         swap = true;
+//       }
+//     }
+//   } while (swap);
+//   return arr;
+// }
+
+function bubbleSort(arr) {
+  let swap = true;
+  while (swap) {
+    swap = false;
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        let temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+        swap = true;
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(bubbleSort([-6, 20, 8, -2, 4]));
